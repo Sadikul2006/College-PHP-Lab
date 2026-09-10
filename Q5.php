7c4fb894fd0d14332d77b370dacb70625571096a
Sadikul2006 <sadikul56@gmail.com>
2026-09-10 20:27:49 +0530

    Add PHP Lab Program 5

---
+++ Q5.php
@@ -0,0 +1,18 @@
+<!-- 
+5. Write a php script to perform simple arithmetic- addition, Minus,
+Multiplication, Division, Modulus of two number
+-->
+
+
+<?php
+
+$num1 = 20;
+$num2 = 5;
+
+echo "Addition = " . ($num1 + $num2) . "<br>";
+echo "Minus = " . ($num1 - $num2) . "<br>";
+echo "Multiplication = " . ($num1 * $num2) . "<br>";
+echo "Division = " . ($num1 / $num2) . "<br>";
+echo "Modulus = " . ($num1 % $num2);
+
+?>
