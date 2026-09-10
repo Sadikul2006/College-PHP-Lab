facc70d13c13939690fecc544bdb5fa059a24e2b
Sadikul2006 <sadikul56@gmail.com>
2026-09-10 20:10:21 +0530

    Add PHP Lab Program 3

---
+++ Q3.php
@@ -0,0 +1,22 @@
+<!-- 
+3. Write a php script to display the following output using string variable and
+concatenation of strings:
+The truth is rarely pure
+It is often said that “The truth is rarely pure”.
+“The truth is rarely pure and never simple” Oscar Wilde
+ -->
+
+ 
+<?php
+
+$str1 = "The truth is rarely pure";
+$str2 = "It is often said that \"The truth is rarely pure\".";
+$str3 = "\"The truth is rarely pure and never simple\" Oscar Wilde";
+
+echo $str1;
+echo "<br>";
+echo $str2;
+echo "<br>";
+echo $str3;
+
+?>
