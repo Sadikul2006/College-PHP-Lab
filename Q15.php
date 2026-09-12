9d02d65d55471f147f33419578d75f99835d2f3f
Sadikul2006 <sadikul56@gmail.com>
2026-09-12 11:32:16 +0530

    Add PHP Lab Program 15

---
+++ Q15.php
@@ -0,0 +1,21 @@
+<!-- 15. Write a php program to implement method overloading. -->
+
+<?php
+
+class Calculator
+{
+    function add(...$numbers)
+    {
+        return array_sum($numbers);
+    }
+}
+
+$obj = new Calculator();
+
+echo "Addition of 2 numbers: " . $obj->add(10, 20);
+echo "<br>";
+
+echo "Addition of 3 numbers: " . $obj->add(10, 20, 30);
+
+?>
+
